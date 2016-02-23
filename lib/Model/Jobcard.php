1cdfa8a03a2b5f990a998aa3a331db4cedebb6b2
anshusushil <milkysushil1920@gmail.com>
2016-02-23 18:43:10 +0530

    correction jobcard

--- lib/Model/Jobcard.php
+++ lib/Model/Jobcard.php
@@ -8,14 +8,9 @@ class Model_Jobcard extends \xepan\base\Model_Document{
 		parent::init();
 
 		$job_j = $this->join('jobcard.document_id');
-<<<<<<< HEAD
-		$this->hasOne('xepan\base\contact','contact_id');
+		$job_j->hasOne('xepan\base\contact','contact_id');
 		$job_j->addField('name');
 		
-=======
-		$job_j->hasOne('xepan\base\commerce\Order','order_id');
-		$job_j->addField('name');
->>>>>>> b88d1718ec9b7af96e40d20967b38f7deb2ac890
 	
 	}
 }

4745774efa8f4b285f6af6909c906317a6f59ff4
anshusushil <milkysushil1920@gmail.com>
2016-03-14 15:24:31 +0530

    modified page and jade of production

--- page/jobcarddetail.php
+++ page/jobcarddetail.php
@@ -14,7 +14,7 @@ class page_jobcarddetail extends \Page {
 		$job_document = $this->add('xepan\hr\View_Document',['action'=> $action],null,['view/jobcard/detail']);
 		$job_document->setIdField('document_id');
 		$job_document->setModel($job_model,
-			['jobcard_no','name','create_date','due_date','date','order_name','current_department','day'],
+			['jobcard_no','type','name','create_date','due_date','date','order_name','current_department','day'],
 			['jobcard_no','name','create_date','due_date','date','order_name','current_department','day']
 			);
 

54e7036c0e4ecca719c9385f18b60328133b62b8
sinharakesh <rksinha.btech@gmail.com>
2016-05-16 12:21:40 +0530

    jobcard detail crated

--- page/jobcarddetail.php
+++ page/jobcarddetail.php
@@ -8,19 +8,27 @@ class page_jobcarddetail extends \xepan\base\Page {
 
 	function init(){
 		parent::init();
+
 		$doc_id=$this->api->stickyGET('document_id');
 
-		$action = $this->api->stickyGET('action')?:'view';
+		$action = $this->api->stickyGET('action')?'view':'view';
+
 		$job_model = $this->add('xepan\production\Model_Jobcard')->tryLoadBy('id',$doc_id);
-						
+		$customer_model = $this->add('xepan\base\Model_Contact')->tryLoadBy('id',$job_model['customer_id']);
+		$order_model = $this->add('xepan\commerce\Model_QSP_Master')->tryLoadBy('id',$job_model['order_no']);
+		
+
 		$job_document = $this->add('xepan\hr\View_Document',['action'=> $action],null,['view/jobcard/detail']);
 
-		$job_document->setIdField('document_id');
-		
-		$job_document->setModel($job_model,
-			['jobcard_no','type','document_no','create_date','due_date','date','order_name','current_department','day'],
-			['jobcard_no','document_no_id','create_date','due_date','date','order_name','current_department','day']
-			);
+		$job_document->setIdField('document_id');		
+		$job_document->setModel($job_model);
+
+		$job_document->template->trySet('address',$customer_model['address']);
+		$job_document->template->trySet('city',$customer_model['city']);
+		$job_document->template->trySet('state',$customer_model['state']);
+		$job_document->template->trySet('country',$customer_model['country']);
+
+		$job_document->template->trySet('order_created_at',$order_model['created_at']);
 
 	}
 }

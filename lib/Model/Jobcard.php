ce4e7db0728911eca9dd625887b71dcff0fe6175
vaibhav <vaibhavsharma38@gmail.com>
2016-06-18 17:58:23 +0530

    fields deleted from search string function

--- lib/Model/Jobcard.php
+++ lib/Model/Jobcard.php
@@ -614,15 +614,9 @@ class Model_Jobcard extends \xepan\base\Model_Document{
 
 		$search_string = ' ';
 		$search_string .=" ". $this['order_no'];
-		$search_string .=" ". $this['customer_id'];
 		$search_string .=" ". $this['customer_name'];
 		$search_string .=" ". $this['order_item_name'];
 		$search_string .=" ". $this['order_item_quantity'];
-		$search_string .=" ". $this['days_elapsed'];
-		$search_string .=" ". $this['toreceived'];
-		$search_string .=" ". $this['processing'];
-		$search_string .=" ". $this['forwarded'];
-		$search_string .=" ". $this['completed'];
 		$search_string .=" ". $this['type'];
 
 		$this['search_string'] = $search_string;
--- lib/Model/OutsourceParty.php
+++ lib/Model/OutsourceParty.php
@@ -125,26 +125,20 @@ class Model_OutsourceParty extends \xepan\base\Model_Contact{
 		if($this->loaded()){
 			$qsp_master = $this->ref('QSPMaster');
 			foreach ($qsp_master as $all_qsp_detail) {
-				$search_string .=" ". $all_qsp_detail['qsp_master_id'];
 				$search_string .=" ". $all_qsp_detail['document_no'];
 				$search_string .=" ". $all_qsp_detail['from'];
 				$search_string .=" ". $all_qsp_detail['total_amount'];
 				$search_string .=" ". $all_qsp_detail['gross_amount'];
 				$search_string .=" ". $all_qsp_detail['net_amount'];
 				$search_string .=" ". $all_qsp_detail['narration'];
-				$search_string .=" ". $all_qsp_detail['exchange_rate'];
-				$search_string .=" ". $all_qsp_detail['tnc_text'];
 			}
 
 			$jobcard = $this->ref('xepan\production\Jobcard');
 			foreach ($jobcard as $jobcard_detail) {
 				$search_string .=" ". $jobcard_detail['order_no'];
-				$search_string .=" ". $jobcard_detail['customer_id'];
 				$search_string .=" ". $jobcard_detail['customer_name'];
 				$search_string .=" ". $jobcard_detail['order_item_name'];
 				$search_string .=" ". $jobcard_detail['order_item_quantity'];
-				$search_string .=" ". $jobcard_detail['days_elapsed'];
-				$search_string .=" ". $jobcard_detail['forwarded'];
 			}
 		}
 

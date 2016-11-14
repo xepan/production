2c7f7f64853b4f9677823c506c988e5f1ce35863
vijay mali <guddavijay@gmail.com>
2016-11-14 14:23:29 +0530

    prodcution jobcard email configuration added

---
+++ page/config.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace xepan\production;
+
+/**
+* 
+*/
+class page_config extends \xepan\base\Page{
+	public $title = "Jobcard Configuration";
+	function init(){
+		parent::init();
+		$config_m = $this->add('xepan\base\Model_ConfigJsonModel',
+		[
+			'fields'=>[
+						'subject'=>'Line',
+						'body'=>'xepan\base\RichText',
+						],
+				'config_key'=>'PRODUCTION_JOBCARD_SYSTEM_CONFIG',
+				'application'=>'production'
+		]);
+		$config_m->add('xepan\hr\Controller_ACL');
+		$config_m->tryLoadAny();
+
+		$form=$this->add('Form',null,'jobcard-received');
+		$form->setModel($config_m,['subject','body']);
+		$form->getElement('subject')->set($config_m['subject'])->setFieldHint(' ')->setCaption('Subject');
+		$form->getElement('body')->set($config_m['body'])->setFieldHint(' ')->setCaption('Message');
+
+		$form->addSubmit('Update')->addClass('btn btn-primary');
+
+		if($form->isSubmitted()){
+			$form->save();
+			$form->js(null,$form->js()->reload())->univ()->successMessage('Update Information')->execute();
+		}
+
+	}
+
+	function defaultTemplate(){
+		return ['page/config'];
+	}
+}

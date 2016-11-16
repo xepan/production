268294ef8ca56612d97dd4572072aff51562a010
sinharakesh <rksinha.btech@gmail.com>
2016-11-16 13:53:19 +0530

    view pipeline in develop

---
+++ lib/View/JobcardPipeline.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace xepan\production;
+
+class View_JobcardPipeline extends \CompleteLister{
+	public  $order_detail_id;
+	
+	function formatRow(){
+
+		$jobcard = $this->add('xepan\production\Model_Jobcard');
+		$jobcard->addCondition('order_item_id',$this->order_detail_id);
+		$jobcard->addCondition('department_id',$this->model->id);
+		$jobcard->tryLoadAny();
+
+		$this->current_row_html['jobcard_no'] = $jobcard['id'];
+		$this->current_row_html['status'] = $jobcard['status'];
+		$this->current_row_html['created_at'] = $jobcard['created_at'];
+		parent::formatRow();
+	}
+
+	function defaultTemplate(){
+		return['view\jobcardpipeline'];
+	}
+}
